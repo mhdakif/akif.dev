457af7c6b301e8b539cd1a13dbad207fc7a3ee28
mhdakif <mhdaakiiff@gmail.com>
2025-04-09 13:03:33 +0530

    Create linear.c

---
+++ linear.c
@@ -0,0 +1,17 @@
+/* this program is linear search*/
+#include<stdio.h>
+void main()
+{
+  int a[10],n,key;
+printf("enter the size of the array");
+scanf("%d",&n);
+printf("enter the element");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("enter the number to be search");
+scanf("%d",&key);
+for
+
+  
